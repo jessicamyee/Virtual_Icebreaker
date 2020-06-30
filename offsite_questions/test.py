ca4e8cd9e5dd3aeabcdb01bec7bd489108c13f95
Jess <jess.myee@gmail.com>
2020-06-30 00:47:55 -0400

    Added Heroku superuser and set up 2 initial set up tests

---
+++ offsite_questions/test.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+from offsite_questions.models import Entry
+
+# Create your tests here.
+
+class EntryTest(TestCase):
+    def test_setUp(self):
+        #Setup run before every test method.
+        pass
+
+    def test_tearDown(self):
+        #Clean up run after every test method.
+        pass
+
