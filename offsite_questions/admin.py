642cb3c79c5ce9066fb937d671afe4549ce2cbe9
Jess <jess.myee@gmail.com>
2020-07-12 15:14:30 -0400

    Allow Administrator to export database to json, csv, etc

--- offsite_questions/admin.py
+++ offsite_questions/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
-
+from .models import Entry
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 from offsite_questions.models import Entry
+
+
 
 # Register your models here.
 
-admin.site.register(Entry)
+# admin.site.register(Entry)
+
+class EntryResource(resources.ModelResource):
+    class Meta:
+        model = Entry
+
+
+class EntryAdmin(ImportExportModelAdmin):
+    resource_class = EntryResource
+
+admin.site.register(Entry, EntryAdmin)            
--- offsite_questions/views.py
+++ offsite_questions/views.py
@@ -1,11 +1,6 @@
 from django.shortcuts import render
 from offsite_questions.models import Entry
 
-# <QueryDict: {
-# 'csrfmiddlewaretoken': ['l3PWk42bp48JRSuhhN9mWpJcwg81JaGuxiolRiJdliXtMWHBiBKyKXg0cwSlxzQQ'],
-# 'question_number': ['30'],
-# 'submit': ['']}
-# >
 
 def index(request):
     """The home page for Hotsprings Questions"""
@@ -23,8 +18,8 @@ def index(request):
         context = {'question': question_text}
 
     return render(request, 'q2_hotsprings_temps/index.html', context)
+
  
-##Add some code where the first letter of the first word in the hotspring question is capitalized --- will also be its own unit test
-##Ensuring submission integers are between 1-25 
-##Ensuring that we cannot add a question number when the number has been added
-##Will need to write another function in index.html where if a user submits a new hotsprings question, the min and max will increase by increment of 1 and the HTML hard-coding will not be static --- will unit test
+##Allowing users to add additional questions w/o needing to input a number. THe backend will just add an unused number ID to the new question
+# ##Ensuring that we cannot add a question number when the number has been added
+
