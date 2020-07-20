a0f6b4f7f8ae89c7ad5f2f5404f2ef01e6935b06
Jess <jess.myee@gmail.com>
2020-07-20 00:30:19 -0400

    Modified process of retrieving question

--- offsite_questions/models.py
+++ offsite_questions/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import random
 
 # Create your models here.
 
@@ -9,9 +10,20 @@ class Entry (models.Model):
 
     def __str__(self):
         """Return a string representation of the model."""
-        return str(self.question_number) + " " + self.question_text
+        return str(self.question_text)
     
     @staticmethod
     def total_count():
         """Returns the total number of entries available."""
         return Entry.objects.all().count()
+
+    @staticmethod
+    def question_list():
+        """Returns the list of questions."""
+        return Entry.objects.all()
+
+    @staticmethod
+    def random_question():
+        """Returns a random question."""
+        return random.choice(Entry.question_list())
+
--- offsite_questions/views.py
+++ offsite_questions/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from offsite_questions.models import Entry
+import random
 
 
 def index(request):
@@ -8,18 +9,12 @@ def index(request):
     #GET request, hit the page for the first time
     if request.method == 'GET':
         context = {'question': ''}
+      
 
     #POST request, the user has submitted something via the form
     elif request.method == 'POST':
         data = request.POST
-        entered_question_number = int(data['question_number'])
-
-        question_text = Entry.objects.get(question_number = entered_question_number).question_text
-        context = {'question': question_text}
+        context = {'question': Entry.random_question()} 
 
     return render(request, 'q2_hotsprings_temps/index.html', context)
-
- 
-##Allowing users to add additional questions w/o needing to input a number. THe backend will just add an unused number ID to the new question
-# ##Ensuring that we cannot add a question number when the number has been added
 
