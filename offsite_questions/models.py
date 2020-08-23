d9cb419824bd1b83ffa205a9b10af13ffc29c039
Jess <jess.myee@gmail.com>
2020-08-23 16:46:34 -0400

    Revert "Added multi tenancy to models"
    This reverts commit 638f8993862bc487db18e5ecdf3fd1a6fd27c710.

--- offsite_questions/models.py
+++ offsite_questions/models.py
@@ -3,17 +3,7 @@ import random
 
 # Create your models here.
 
-class Tenant(models.Model):
-    name = models.CharField(max_length=100)
-    subdomain_prefix = models.CharField(max_length=100, unique=True)
-
-class TenantAwareModel(models.Model):
-    tenant = models.ForeignKey(Tenant, on_delete=models.CASCADE)
-
-    class Meta:
-        abstract = True    
-
-class Entry (TenantAwareModel):
+class Entry (models.Model):
     """Entry of a hotsprings question"""
     question_number = models.IntegerField()
     question_text = models.CharField(max_length=200)
