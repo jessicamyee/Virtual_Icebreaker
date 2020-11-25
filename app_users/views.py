98d1c7c781843cef5b4f3243da3e15adad279a6a
Jess <jess.myee@gmail.com>
2020-11-25 08:47:58 -0500

    Extended user model to include team name field during registration

--- app_users/views.py
+++ app_users/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth import login, logout, authenticate
-from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.forms import UserCreationForm
+from app_users.forms import Registration
 
 # Create your views here.
 
@@ -17,10 +18,11 @@ def register(request):
     """Register a new user."""
     if request.method != 'POST':
         #Display blank registration form.
-        form = UserCreationForm() 
+        form = Registration() 
+
     else:
         #Process completed form.
-        form = UserCreationForm(data=request.POST)
+        form = Registration(data=request.POST)
 
         if form.is_valid():
             new_user = form.save()
@@ -31,4 +33,4 @@ def register(request):
 
     context = {'form': form}
     return render(request, 'app_users/register.html', context)                   
-
+    
